c3030d6e241d438f044c82794a850ee281e592fd
odaniel9896 <soumenorgame@gmail.com>
2021-04-05 12:26:37 -0300

    Adicionando Login e rota

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const express = require("express");
 //IMPORT DOS CONTROLLERS
 
 const studentController = require("./controller/students");
+const sessionController =  require("./controller/sessions")
 
 //IMPORT DOS MIDDLEWARE
 
@@ -14,6 +15,10 @@ const routes = express.Router();
 //Rotas para o Student
 routes.get("/students", studentController.index);
 routes.post("/students", studentController.store);
+
+
+//ROTAS PARA A SEÇÃO
+routes.post("/login", sessionController.store);
 
 //Verificar cadastro
 routes.get('/verify', emailMiddleware.verifyEmail);
