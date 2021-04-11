4df752e5720456381533147d43bd308f62c581f6
odaniel9896 <soumenorgame@gmail.com>
2021-04-11 08:39:39 -0300

    adicionado cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 //import do express
 const express = require("express");
+const cors = require("cors");
+
 
 const routes = require("./routes");
+
 
 require("./database");
 
@@ -9,6 +12,8 @@ require("./database");
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use(routes);
 
