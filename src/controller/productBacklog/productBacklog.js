fdb8bb7b7519593876089a987de7e0f7ce3e5f0f
O Daniel <soumenorgame@gmail.com>
2021-05-27 17:07:01 -0300

    Criação de storie finalizada

--- src/controller/productBacklog/productBacklog.js
+++ src/controller/productBacklog/productBacklog.js
@@ -1,12 +1,27 @@
+const { findGroupById } = require("../../repositories/group");
+
 module.exports = {
     async store(req, res) {
-        
+
         const groupId = req.params.groupId
 
+        const { description, priority } = req.body;
+
         try {
+            const group = await findGroupById(groupId);
+
+            if (!group)
+                return res.status(404).send({ error: "Grupo não encontrado" });
+
+            const storie = await group.createProductBacklog({
+                description : description,
+                priorityId: priority
+            });  
             
+            res.status(201).send(storie)
         } catch (error) {
-            
+            console.log(error);
+            res.status(500).send(error);
         }
     }
 }
