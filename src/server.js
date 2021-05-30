7359e1a8a180315328d43861eea654fbbfcd0ea2
O Daniel <soumenorgame@gmail.com>
2021-05-30 11:44:04 -0300

    Sistema de chat funcionando

--- src/server.js
+++ src/server.js
@@ -1,7 +1,59 @@
 const app = require("./app");
+const app2 = require("./app");
+
+const socket = require("socket.io");
+const Message = require("./models/Message");
+
 
 const PORT = 3333;
+const PORT2 = 3002;
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`)
-})
+});
+
+const serverWebsocket = app.listen(PORT2, () => {
+    console.log("Servidor rodando na porta 3002")
+});
+
+const io = socket(serverWebsocket);
+
+io.on("connection", async (socket) => {
+    console.log(socket.id);
+
+    socket.on("join_room", (data) => {
+        socket.join(data);
+        console.log("User Joined Room: " + data);
+    });
+
+    socket.on("send_message", async (data) => {
+        // console.log(data);
+        // console.log(data.content.message);
+        const message = await Message.create({
+            text: data.content.message,
+            userId: data.userId,
+            groupId: data.groupId,
+            chatId: data.chatId,
+        });
+        socket.to(data.room).emit("receive_message", data.content);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("USER DISCONNECTED");
+    });
+
+    socket.on("delete_message", async (data) => {
+
+        const message = await Message.findOne({
+            where: {
+                id: data.content.id,
+                userId: data.content.userId
+            }
+        });
+
+        if (!message)
+            return res.status(401).send({ error: "Mensagem não encontrada" })
+        message.destroy();
+    })
+});
+
