af1de24f285e66c48d230c9e34ec46c7c0552e06
Daniel Vitor <soumenorgame@gmail.com>
2021-06-22 16:51:35 -0300

    adicionando descrição

--- src/controller/card/cardsInfo.js
+++ src/controller/card/cardsInfo.js
@@ -39,7 +39,7 @@ module.exports = {
         where: {
           id: cardId,
         },
-        attributes: ["id", "initialDate", "dueDate", "updatedAt", "createdAt"],
+        attributes: ["id", "description", "initialDate", "dueDate", "updatedAt", "createdAt"],
         include: [
           {
             association: "Progress",
